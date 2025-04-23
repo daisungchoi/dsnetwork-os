b3ab7b4e530ddf759bc54882bc2f500abc58219c
daisungchoi <daisungchoi@yahoo.com>
2025-04-23 10:24:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,14 @@ resource "aws_vpc" "main" {
 # Enable EC2 Instances
 
 # Enable Public Subnets
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
 
 # Enable Private Subnets
 
