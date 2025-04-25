384ecdbb8df4ccb95be8cd672d7bfef356a39338
daisungchoi <daisungchoi@yahoo.com>
2025-04-24 23:17:17 -0400

    Update main.tf
    added private subnet

--- main.tf
+++ main.tf
@@ -26,6 +26,14 @@ resource "aws_subnet" "main" {
 }
 
 # Enable Private Subnets
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
 
 # Enable Public Route Tables
 
