39f2d20d8dec56735854c3e95e3a2c8704d958de
daisungchoi <daisungchoi@yahoo.com>
2025-04-29 22:39:09 -0400

    Update main.tf
    Added public route table

--- main.tf
+++ main.tf
@@ -36,6 +36,14 @@ resource "aws_subnet" "main" {
 }
 
 # Enable Public Route Tables
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
 
 # Enable Private Route Tables
 resource "aws_route_table" "main" {
